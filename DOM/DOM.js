1c071d670b05534245f05e9d291865e911c09c83
Hardik Hadiya <hardikhadiya21@gmail.com>
2025-07-03 17:56:45 +0530

    Refactor login function to improve variable naming and add user existence check

---
+++ DOM/DOM.js
@@ -0,0 +1,13 @@
+let a = document.getElementById("id");
+console.dir(a);
+
+let b = document.getElementsByClassName("dom");
+console.dir( b);
+
+let c = document.getElementsByTagName("div");
+console.dir(c);
+
+let d = document.querySelector(".dom");//it only select the first element
+
+let e = document.querySelectorAll(".dom");//it select all elements
+
