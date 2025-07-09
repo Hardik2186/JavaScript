651a731cfccd80f7d14c15433a7b9b6e1dcc62e2
Hardik Hadiya <hardikhadiya21@gmail.com>
2025-07-09 18:19:41 +0530

    1.0

--- Events/script.js
+++ Events/script.js
@@ -1,6 +1,33 @@
-let btn = document.getElementById("btn2");
+let btn2 = document.getElementById("btn2");
 
-btn.ondblclick = () => {
+btn2.ondblclick = () => {
     console.log('Double Click successfully.'); 
     alert('Button clicked successfully..');
 }
+
+let btn1 = document.getElementById("btn1");
+
+
+//event handling
+btn1.onclick= (e) => {
+    console.log('Click successfully.'); 
+    alert('Button clicked successfully..')
+    
+    console.log(e.target);
+    console.log(e.type);
+    console.log(e.clientX);
+    console.log(e.clientY);
+
+}
+
+//addEventListener(event,callback)
+
+const Handle = () => {
+    console.log("button clicked successfully :)");
+}
+
+btn1.addEventListener("click",Handle)
+
+//removeEventListener(event,callback)
+
+btn1.removeEventListener("click",Handle)
