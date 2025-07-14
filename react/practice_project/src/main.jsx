47c573f320743deeaea2deed1ca95b96e0a3c3a3
Hardik Hadiya <hardikhadiya21@gmail.com>
2025-07-14 18:27:30 +0530

    1.0

---
+++ react/practice_project/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+const myElement = <h1>Hello</h1>
+
+createRoot(document.getElementById('root')).render(
+  myElement
+)
